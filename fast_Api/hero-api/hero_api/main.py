2b09b071169bf149c7f6a5cd7b32c3343a0250e6
Faizan59khan <khan_faizan59@yahoo.com>
2024-04-14 10:27:33 +0500

    Implement Fastapi concepts(response model, multplie models, crud operations)

--- fast_Api/hero-api/hero_api/main.py
+++ fast_Api/hero-api/hero_api/main.py
@@ -1,11 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,  Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import List, Optional
 
-class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+
+from contextlib import asynccontextmanager
+
+
+
+
+class HeroBase(SQLModel):
     name: str = Field(index=True)
     secret_name: str
     age: int | None = Field(default=None, index=True)
+
+
+class Hero(HeroBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class HeroCreate(HeroBase):
+    pass
+
+
+class HeroPublic(HeroBase):
+    id: int
+
+class HeroUpdate(SQLModel):
+    name: str | None = None
+    secret_name: str | None = None
+    age: int | None = None
+
 
 
 sqlite_file_name = "hero_db.db"
@@ -17,27 +41,54 @@ engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-# Code above omitted 👆
-
-app = FastAPI()
 
 
-@app.on_event("startup")
-def on_startup():
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
-@app.post("/heroes/")
-def create_hero(hero: Hero):
+
+#response_model=Hero is output response schema
+@app.post("/heroes/", response_model=HeroPublic)
+def create_hero(hero: HeroCreate):
     with Session(engine) as session:
-        session.add(hero)
+        db_hero = Hero.model_validate(hero)
+        session.add(db_hero)
         session.commit()
-        session.refresh(hero)
+        session.refresh(db_hero)
+        return db_hero
+
+
+@app.get("/heroes/", response_model=list[HeroPublic])
+def read_heroes(offset: int = 0, limit: int = Query(default=100, le=100)):
+    with Session(engine) as session:
+        heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+        return heroes
+
+@app.get("/heroes/{hero_id}", response_model=HeroPublic)
+def read_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
         return hero
 
-
-@app.get("/heroes/")
-def read_heroes():
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate):
     with Session(engine) as session:
-        heroes = session.exec(select(Hero)).all()
-        return heroes
+        db_hero = session.get(Hero, hero_id)
+        if not db_hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        hero_data = hero.model_dump(exclude_unset=True)
+        db_hero.sqlmodel_update(hero_data)
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+        return db_hero
